bbea134bc1874fadf32a580f76cff2e005a21f51
westin <>
2020-12-13 20:51:00 -0600

    Cleaned up code and added comments

--- controllers/RobotDriver/RobotDriver.py
+++ controllers/RobotDriver/RobotDriver.py
@@ -4,51 +4,105 @@
 #  from controller import Robot, Motor, DistanceSensor
 
 import os
-import math
 import sys
 import json
-import numpy as np
 
+import controller
+
+#Set up the import path for ennlib.
 sys.path.insert(0, os.path.join(os.path.split(os.path.realpath(__file__))[0], ".."))
 from ennlib import BasicNeuralNet
 
 
-import controller
 
 def scale(val, in_range, out_range):
     """
+    Scales a value between one range and another.
     in_range should be of format (in_min, in_max)
     out_range should be of format (out_min, out_max)
     """
     ret = (val-in_range[0])/(in_range[1]-in_range[0])*(out_range[1]-out_range[0])+out_range[0]
-    # print(val, ret, in_range, out_range)
     return ret
 
 
 class MotorInfo:
+    """
+    Structure for storing a reference to a motor on a robot and the type.
+    """
     POSITION = 1
     VELOCITY = 2
 
     def __init__(self, motor):
+        """
+        Stores the motor and type.
+        """
         self.motor = motor
         pos_range = (motor.getMinPosition(), motor.getMaxPosition())
+        #Webots indicates a motor has infinite rotation by setting its min and max position to 0.
         if pos_range[0] == 0 and pos_range[1] == 0:
+            #This is an infinite rotation motor. We want to control it using velocity rather than position.
             self.mode = self.VELOCITY
-            max_v = motor.getMaxVelocity()
+            #In order to set a motor to velocity mode, Webots requires us to set its position to infinity.
             motor.setPosition(float("inf"))
+            #The range of allowed values is negative to positive max speed.
+            #This speed is in rads/sec for rotational motors and meters/sec for linear motors.
+            max_v = motor.getMaxVelocity()
             range = (-max_v, max_v)
         else:
+            #This is not an infinite rotation motor. Control it with position.
             self.mode = self.POSITION
             range = pos_range
+        #There can be issues with going right up to the limits (floating point error?), so we make the range slightly smaller.
         self.range = (scale(0.001, (0,1), range), scale(0.999, (0,1), range))
 
 
-# create the Robot instance.
+
+def set_joint_values(motor_infos, joint_values):
+    """
+    Sets motor values for both velocity controlled and position controlled joints.
+    MotorInfo is a list of MotorInfo objects.
+    joint_values should be a list of values between 0 and 1 where there is one joint value per motor.
+    """
+    assert len(motor_infos) == len(joint_values), f"Invalid joint count: Expected {len(motor_infos)} but got {len(joint_values)}."
+
+    #Iterate over each motor and joint value.
+    for motor_info, joint_value in zip(motor_infos, joint_values):
+        #Scale the value to the appropriate range.
+        joint_value = scale(joint_value, (0, 1), motor_info.range)
+
+        #Depending on the type of motor, set the value differently.
+        if motor_info.mode == MotorInfo.POSITION:
+            #The motor is position controlled. Set position.
+            motor_info.motor.setPosition(joint_value)
+
+        elif motor_info.mode == MotorInfo.VELOCITY:
+            #The motor is velocity controlled. Set the velocity.
+            motor_info.motor.setVelocity(joint_value)
+
+        else:
+            #Something went wrong.
+            raise Exception("Invalid motor mode:", motor_info.mode)
+
+def get_joint_values(motors):
+    #Gets the values from the motor (either position or velocity) and scales it to a value between 0 and 1.
+    get_val = lambda motor_info: motor_info.motor.getTargetPosition() if motor_info.mode is MotorInfo.POSITION else motor_info.motor.getVelocity()
+    return [scale(get_val(motor_info), motor_info.range, (0, 1)) for motor_info in motors]
+
+
+#Bring in the arguments.
+if len(sys.argv) < 2 or sys.argv[1] == "":
+    print("No network received in parameter.")
+    sys.exit(0)
+else:
+    net = BasicNeuralNet.from_dict(json.loads(sys.argv[1]))
+
+#Create the Robot instance.
 robot = controller.Robot()
 
-# get the time step of the current world.
+#Get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
+#Gather the motors.
 motors = []
 for i in range(robot.getNumberOfDevices()):
     device = robot.getDeviceByIndex(i)
@@ -56,45 +110,20 @@ for i in range(robot.getNumberOfDevices()):
         motors.append(MotorInfo(device))
 
 
-def set_joint_values(motor_infos, joint_values):
-    assert len(motor_infos) == len(joint_values), f"Invalid joint count: Expected {len(motor_infos)} but got {len(joint_values)}."
+#In addition to sending the motor values as inputs to the neural network, a timer value
+#is passed through as well in hopes of encouraging the network to have a cyclic pattern.
+#This signal is a triangle wave and these are the parameters.
+MOVE_PERIOD_TIME_SECS = 1  # Defines the period of the clock value signal to the NN.
+CLOCK_VAL_AMPLITUDE = 1  # Defines the maximum value for the clock signal to the NN.
 
-    for motor_info, joint_value in zip(motor_infos, joint_values):
-        joint_value = scale(joint_value, (0, 1), motor_info.range)
-
-        if motor_info.mode == MotorInfo.POSITION:
-            motor_info.motor.setPosition(joint_value)
-
-        elif motor_info.mode == MotorInfo.VELOCITY:
-            motor_info.motor.setVelocity(joint_value)
-
-        else:
-            raise Exception("Invalid motor mode:", motor_info.mode)
-
-def get_joint_values(motors):
-    get_val = lambda motor_info: motor_info.motor.getTargetPosition() if motor_info.mode is MotorInfo.POSITION else motor_info.motor.getVelocity()
-    return [scale(get_val(motor_info), motor_info.range, (0, 1)) for motor_info in motors]
-
-
-if len(sys.argv) < 2 or sys.argv[1] == "":
-    print("No network received in parameter.")
-    sys.exit(0)
-else:
-    net = BasicNeuralNet.from_dict(json.loads(sys.argv[1]))
-
-
-
-# Main loop:
-MOVE_PERIOD_TIME_SECS = 1
-CLOCK_VAL_AMPLITUDE = 1
+#Main loop
 while robot.step(timestep) != -1:
+    #Calculate the timer value.
+    timer_input = (robot.getTime() % MOVE_PERIOD_TIME_SECS) * CLOCK_VAL_AMPLITUDE
 
-    cycle_start_time = robot.getTime()
-    timer_input = ((cycle_start_time)%MOVE_PERIOD_TIME_SECS) * CLOCK_VAL_AMPLITUDE
-
+    #Get the current state of the joints.
     joint_inputs = get_joint_values(motors)
 
+    #Send the inputs through the neural network and set joints using its outputs.
     inputs = [timer_input] + joint_inputs
     set_joint_values(motors, net.process_network(inputs))
-
-# Enter here exit cleanup code.
--- controllers/SupervisorController/SupervisorController.py
+++ controllers/SupervisorController/SupervisorController.py
@@ -1,71 +1,77 @@
-"""SupervisorController controller."""
+"""
+SupervisorController controller.
+
+One robot with the supervisor bit must be set and running this controller in order to
+execute the EA on the other robots.
+"""
+
 
 import argparse
-import datetime
 import json
 import os
 import random
 import sys
 import time
 
-# You may need to import some classes of the controller module. Ex:
-#  from controller import Robot, Motor, DistanceSensor
 from controller import Supervisor
 import controller
 
-
+#Add to the import path in order to reference ennlib.
 sys.path.insert(0, os.path.join(os.path.split(os.path.realpath(__file__))[0], ".."))
 from ennlib import EASupervisor
 
 
 
-
-
-
-ROBOT_CONTROLLER_NAME = "RobotDriver"
-
-# create the Robot instance.
-supervisor = Supervisor()
-
-# get the time step of the current world.
-timestep = int(supervisor.getBasicTimeStep())
-
-
-
-
-#https://www.cyberbotics.com/doc/reference/supervisor#wb_supervisor_field_import_mf_node
-
 class RobotContainer:
+    """
+    Container for storing a robot, its neural network, and the start pose of the robot.
+    """
     def __init__(self, robot, start_coord, net_wrapper=None):
         self.robot = robot
         self.start_coord = start_coord
         self.net_wrapper = net_wrapper
 
 
-
-
-
-
-###TODO: MAKE SURE ALL ROBOTS ARE THE SAME!
 def initialize_robots():
+    """
+    Initializes a RobotContainer object for each robot found in the environment and returns them in a list.
+    """
+    global supervisor
+
     robot_containers = []
     root_children = supervisor.getRoot().getField("children")
+    #Iterate through all of the top level things in the environment looking for robots.
     for i in range(root_children.getCount()):
         node = root_children.getMFNode(i)
+        #Check the type of node we found to see if it is a robot of interest (a robot and not a supervisor)
         if node.getType() == node.ROBOT and not node.getField("supervisor").getSFBool():
             #We found a robot.
             start_coord = node.getField("translation").getSFVec3f()
             robot_containers.append(RobotContainer(node, start_coord))
     return robot_containers
 
+
 def pause():
+    """
+    Pauses the simulation
+    """
     supervisor.simulationSetMode(supervisor.SIMULATION_MODE_PAUSE)
 
+
 def run():
+    """
+    Puts the simulation in run mode.
+    """
     supervisor.simulationSetMode(supervisor.SIMULATION_MODE_RUN)
 
 
 def prepare_cycle(robot_containers, net_wrappers):
+    """
+    Assigns neural networks to each robot.
+    robot_containers must be a list of RobotContainer objects as created by initialize_robots().
+    net_wrappers is the list of NetWrapper objects passed into the EA's fitness function callback.
+    The number of robots and networks must be equal.
+    """
     assert len(robot_containers) == len(net_wrappers), f"Number of robots and networks must match. {len(robot_containers)}!={len(net_wrappers)}"
     for i, container in enumerate(robot_containers):
         container.net_wrapper = net_wrappers[i]
@@ -83,6 +89,11 @@ def prepare_cycle(robot_containers, net_wrappers):
 
 
 def update_fitnesses(containers):
+    """
+    Calculates the fitness of each robot and updates the fitness within the container.
+    Fitness is equal to the euclidian distance from the robot's starting point to its current position.
+    containers is the list of RobotContainer objects with the networks initialized in them.
+    """
     for i, container in enumerate(containers):
         robot = container.robot
         end_coord = robot.getField("translation").getSFVec3f()
@@ -90,18 +101,28 @@ def update_fitnesses(containers):
 
 
 def count_robot_motors(robot):
+    """
+    Traverses a robot tree to count the number of motors.
+    This works for robots whether or not they are defined by a proto file.
+    """
+    #TODO: Consider adding support for linear actuators.
     def _count_field_motors(field):
         motor_count = 0
+        #Iterate through the field's nodes looking for a hinge joint.
         for i in range(field.getCount()):
             child = field.getMFNode(i)
+            #If the node we found is a hinge joint, check it for a motor after recursing into its end point.
             if child.getType() == child.HINGE_JOINT:
                 end_point = child.getField("endPoint")
+                #If the joint has an end point, recurse into it.
                 if end_point is not None:
                     motor_count += _count_field_motors(end_point.getSFNode().getField("children"))
+                #Check to see if we have a motor.
                 if child.getField("device").getCount() > 0:
                     motor_count += 1
         return motor_count
 
+    #The method to get the robot fields depends on whether or not it was defined by a proto file.
     if robot.isProto():
         children = robot.getProtoField("children")
     else:
@@ -112,19 +133,43 @@ def count_robot_motors(robot):
 
 fitness_cycle_counter = 0
 def fitness_function_callback(new_population, epoch_time):
+    """
+    Fitness function for the EA.
+    This assigns neural networks to the existing robots, runs for the specified
+    amount of time, and then calculates and updates fitnesses.
+    epoch_time defines the amount of time for the EA to run in seconds.
+    """
     global fitness_cycle_counter
+
+    #Assign neural networks to robots.
     prepare_cycle(containers, new_population)
     fitness_cycle_counter += 1
     print(f"Starting cycle {fitness_cycle_counter}.")
+
+    #Start the simulation.
     run()
 
+    #Wait for a while to let the robots move.
     start_time = supervisor.getTime()
     while supervisor.getTime() < (start_time + epoch_time):
         if supervisor.step(timestep) == -1:
             sys.exit()
 
+    #Pause the simulation.
     pause()
+
+    #Calculate the fitnesses.
     update_fitnesses(containers)
+
+
+
+
+#Create the Robot instance.
+supervisor = Supervisor()
+
+#Get the time step of the current world.
+timestep = int(supervisor.getBasicTimeStep())
+
 
 
 if __name__ == "__main__":
@@ -145,18 +190,22 @@ if __name__ == "__main__":
     args = parser.parse_args()
     if args.minweight >= args.maxweight:
         print("Error! minweight must be less than maxweight")
+        parser.print_help()
         exit(1)
 
     if args.minbias >= args.maxbias:
         print("Error! minbias must be less than maxbias")
+        parser.print_help()
         exit(1)
 
     if args.layercount <= 0:
         print("Error! layer count must be greater than 0.")
+        parser.print_help()
         exit(1)
 
     if args.epoch <= 0:
         print("Error! epoch must be greater than 0.")
+        parser.print_help()
         exit(1)
 
     random.seed(args.seed)
@@ -166,16 +215,24 @@ if __name__ == "__main__":
 
 
     print("Starting supervisor")
+
+    #Pause the simulation
     pause()
+
+    #Gather the robots
     containers = initialize_robots()
 
     if len(containers) < 2:
         print("Error! At least 2 robots must be defined!")
+        parser.print_help()
         exit(1)
 
+    #Count the motors so we know how many inputs and outputs we need.
     motor_count = count_robot_motors(containers[0].robot)
     new_generation_size = len(containers)
     nn_output_count = motor_count
     nn_input_count = nn_output_count + 1
+
+    #Create and run the EA supervisor.
     ea = EASupervisor(fitness_function_callback, new_generation_size, nn_input_count, nn_output_count, weight_min_max, bias_min_max, layer_count=args.layercount, output_path=args.outpath, max_mutation_count=args.maxmutationcount, callback_args=[args.epoch])
     ea.run()
